ff993467c5dd74d9389314d151ea22c6742de814
AnasOkasha-chambaam <anasokashachama@gmail.com>
2021-09-16 05:05:32 +0200

    Project finished. UI remains.

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 let projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 // Start up an instance of app
 const app = express();
 /* Middleware*/
-const bodyParser = require('body-parser'),
-cors = require('cors');
+const bodyParser = require("body-parser"),
+  cors = require("cors");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,32 +15,30 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
 const serverPort = 8030;
 
 // *_ Setup routes
-app.get('/tempData',(req,res)=>{ // get route
-    return projectData;
-})
-app.post('/addData',(req,res)=>{
-    let incommingRequestBody = req.body;
-    console.log(incommingRequestBody);
-    if(incommingRequestBody.temperature && incommingRequestBody.date){
-        console.log('passed');
-        projectData = {...incommingRequestBody};
-        console.log('jj',projectData)
-    } else {
-        return 'Something wrong with the submitted info. Please, resubmit it.'
-    }
-})
+app.get("/tempData", (req, res) => {
+  // get route
+  res.send(projectData);
+});
+app.post("/addData", (req, res) => {
+  let incommingRequestBody = req.body;
+  console.log(incommingRequestBody);
+  if (incommingRequestBody.temperature && incommingRequestBody.newDate) {
+    console.log("passed");
+    projectData = { ...incommingRequestBody };
+    console.log("jj", projectData);
+    res.send(true);
+  } else {
+    res.send("Something wrong with the submitted info. Please, resubmit it.");
+  }
+});
 
-
-
-
-
-let startServer = app.listen(serverPort,()=>{ // start the server on the specified port
-    console.log("The server ran successfully on localhost:"+port);
+let startServer = app.listen(serverPort, () => {
+  // start the server on the specified port
+  console.log("The server ran successfully on localhost:" + serverPort);
 });
--- website/app.js
+++ website/app.js
@@ -1,5 +1,79 @@
 /* Global Variables */
-
+const openWeatherAppApiKey = "d393766ea183b145c44a75f5a49e4d34",
+  openWeatherMapURL = "https://api.openweathermap.org/data/2.5/weather?";
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear(); // *_ getMonth is zero based, which means it strats by zero and ends by 11. So it should be icreased by one.
+
+// functions start
+/**
+ * @description - Fetch Information about temprature using the 'open weather map' website api
+ * @param {String} apiBaseUrl - Base URL of openWeatherMap API
+ * @param {Integer}  userZipCode - User zip code
+ * @param {String} apiKey - API key
+ * @returns {Object} tempInfoResp.json() - The weather information for the zipcode entered by user
+ */
+async function fetchTempInfo(apiBaseUrl, userZipCode, apiKey) {
+  let tempInfoResp = await fetch(
+    `${apiBaseUrl}zip=${userZipCode}&appid=${apiKey}`
+  );
+  return await tempInfoResp.json();
+}
+
+/**
+ * @description - POST temprature that was GOT from openWeatherMap API along with the current date and user response
+ * @param {String} postAPIPath - The path used to post data through.
+ * @param {Object} dataToSend - The object that contains the data which should be sent to the server. These data are 'area temperature', 'current date', 'user response'
+ */
+
+async function postDataToServer(postAPIPath, dataToSend) {
+  let postResp = await fetch(postAPIPath, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataToSend),
+  });
+}
+
+/**
+ * @description - GET the data object from the server and update the UI
+ */
+
+async function updateUserInterface() {
+  let dataResp = await fetch("/tempData"),
+    retrivedDataObject = await dataResp.json();
+  date.innerText = retrivedDataObject.newDate;
+  temp.innerText = retrivedDataObject.temperature;
+  content.innerText = retrivedDataObject.userRespns;
+}
+
+/**
+ * @description - Validate the form then run a chain of promises to get temperature information and update the UI according to it
+ * @param {Object} event - The click event object
+ */
+
+function generateEvnetListener(event) {
+  event.preventDefault();
+  let enteredUserZipCode = zip.value,
+    userRespns = feelings.value;
+  if (!enteredUserZipCode || !userRespns) {
+    console.log("Not valid");
+    return;
+  }
+  fetchTempInfo(openWeatherMapURL, enteredUserZipCode, openWeatherAppApiKey)
+    .then((recievedData) => {
+      let temperature = recievedData.main.temp;
+      postDataToServer("/addData", { temperature, newDate, userRespns });
+    })
+    .then(() => {
+      updateUserInterface();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+// functions end
+// event listeners
+generate.addEventListener("click", generateEvnetListener);
